dafe9a970e9636884314688b0f40ca194f1baf2b
Alfredo Romero <business@alfredoromero.io>
2025-09-26 09:59:46 -0400

    Quitamos el middleware

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -52,9 +52,6 @@ class AdminPanelProvider extends PanelProvider
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
             ])
-            ->authMiddleware([
-                Authenticate::class,
-            ])
             ->authGuard('web')
             ->authPasswordBroker('users');
     }
