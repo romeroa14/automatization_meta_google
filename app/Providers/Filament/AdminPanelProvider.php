c0fba3266c15cc7208f33e5f0e63a158c49d8910
Alfredo Romero <business@alfredoromero.io>
2025-09-26 09:43:38 -0400

    cambios en la autenticacion

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -54,6 +54,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->authGuard('web')
+            ->authPasswordBroker('users');
     }
 }
