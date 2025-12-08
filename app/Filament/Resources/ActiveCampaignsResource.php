4d6ca12ea67c22603ecabaca580d3f7edc212888
alfredogold <alfredoromerox15@gmail.com>
2025-12-08 15:22:12 -0400

    VERIFICACION DE CI/CD

--- app/Filament/Resources/ActiveCampaignsResource.php
+++ app/Filament/Resources/ActiveCampaignsResource.php
@@ -30,7 +30,7 @@ class ActiveCampaignsResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    protected static ?string $navigationLabel = 'Campañas Activassss';
+    protected static ?string $navigationLabel = 'Campañas Activateee menor';
 
     protected static ?string $navigationGroup = 'ADMETRICAS.COM';
 
